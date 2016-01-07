d5027f11c34597217e0cef1fdcb2e9f3e86e5990
steel1990 <lbbsteel@gmail.com>
2016-01-07 19:40:29 +0800

    密码不使用明文填写

--- index.js
+++ index.js
@@ -3,37 +3,43 @@
 var http = require('https');
 var url = require('url');
 var ncp = require('copy-paste');
-var CryptoJS = require("./aes");
 var program = require('commander');
+
+var CryptoJS = require("./aes");
+var pwd = require('./pwd');
 
 var pkg = require('./package.json');
  
 program
-  .version(pkg.version)
-  .option('-p, --pwd [value]', 'password')
-  .option('-d, --data [value]', 'data for send')
-  .parse(process.argv);
+    .version(pkg.version)
+    .option('-d, --data [value]', 'data for send')
+    .option('--clear', 'clear saved password')
+    .parse(process.argv);
 
-if (!program.pwd) {
-    program.help();
-    process.exit();
-}
 
-var option = url.parse('https://crackling-heat-389.firebaseio.com/clipboard.json');
-option.method = program.data ? 'PUT' : 'GET';
+var action = function (password) {
+    var option = url.parse('https://crackling-heat-389.firebaseio.com/clipboard.json');
+    option.method = program.data ? 'PUT' : 'GET';
 
-var req = http.request(option, (res) => {
-    res.on('data', (data) => {
-        data = JSON.parse(data);
-        data = CryptoJS.AES.decrypt(data, program.pwd).toString(CryptoJS.enc.Utf8);
-        ncp.copy(data, () => console.log('copy ' + data + ' suc'));
+    var req = http.request(option, (res) => {
+        res.on('data', (data) => {
+            data = JSON.parse(data);
+            data = CryptoJS.AES.decrypt(data, password).toString(CryptoJS.enc.Utf8);
+            ncp.copy(data, () => console.log('copy ' + data + ' suc'));
+        });
+        res.on('end', () => console.log('suc'));
+        res.on('error', (err) => console.log('error', err));
     });
-    res.on('end', () => console.log('suc'));
-    res.on('error', (err) => console.log('error', err));
-});
 
-if (program.data) {
-    var data = CryptoJS.AES.encrypt(program.data, program.pwd).toString();
-    req.write(`"${data}"`);
+    if (program.data) {
+        var data = CryptoJS.AES.encrypt(program.data, password).toString();
+        req.write(`"${data}"`);
+    }
+    req.end();
+};
+
+if (program.clear) {
+    pwd.clearPassword();
+} else {
+    pwd.getPassword(action);
 }
-req.end();
