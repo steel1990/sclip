1e4c584c81dd97c31c12bcc2d7d2a143f290bb7f
steel1990 <lbbsteel@gmail.com>
2016-01-07 15:20:47 +0800

    添加复制功能

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 
 var http = require('https');
 var url = require('url');
+var ncp = require('copy-paste');
+
+var isCopy = process.argv.length === 2;
 
 var option = url.parse('https://crackling-heat-389.firebaseio.com/clipboard.json');
-option.method = 'PUT';
+option.method = isCopy ? 'GET' : 'PUT'; 
 
 var req = http.request(option, (res) => {
-    res.on('data', (data) => console.log(String(data)));
+    res.on('data', (data) => {
+        data = JSON.parse(data);
+        ncp.copy(data, () => console.log('copy ' + data + ' suc'));
+    });
     res.on('end', () => console.log('suc'));
     res.on('error', (err) => console.log('error', err));
 });
 
 var data = process.argv[process.argv.length - 1];
-req.write(`"${data}"`);
+if (isCopy) {
+    req.write(`"${data}"`);
+}
 req.end();
